5b575ce34a393a65ce01c56f0a38ab5fa00f1e67
Anmol Arora <anmol1610140@akgec.ac.in>
2021-10-25 03:12:54 +0530

    finish coding episode-5

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,31 @@
+import './searchBar.css';
+
+export default function SearchBar(props){
+
+    function submitted(ev){
+        ev.preventDefault();
+        console.log("submitted");
+    }
+
+    function focused(ev){
+        console.log("onFocus");
+    }
+
+    function changed(ev){
+        console.log("on Input", ev.target.value);
+    }
+
+    function clicked(ev){
+        // ev.preventDefault();
+        console.log("clicked");
+    }
+
+    return (
+        <section className="searchBar">
+            <form onSubmit={submitted}>
+                <input type="text" name="keyword" className="searchText" placeholder="keyword" onFocus={focused} onInput={(ev) => changed(ev)} />
+                <button type="submit" className="searchBtn" name="searchBtn" onClick={clicked} >Search</button>
+            </form>
+        </section>
+    )
+}
